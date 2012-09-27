8f41abdeca58ab10b332ca3efab05553fd553217
Ilya Shaisultanov <ilya.shaisultanov@gmail.com>
2012-09-27 15:51:17 -0400

    Added status output to check_changes

--- check_changes.sh
+++ check_changes.sh
@@ -4,7 +4,7 @@ function checkRepos() {
   local cwd=$1
   local files=`ls $cwd`
   local commits="^##\ .+\ \[ahead [0-9]+\]$"
-  local changes="^\ *([MADCUR\?]{1,2})\ .+$"
+  local changes="^\ *([CAR\?MUD\!]{1,2})\ .+$"
 
   for file in $files; do
     if [ -d $cwd/$file ]; then
@@ -16,13 +16,38 @@ function checkRepos() {
       fi
       
       if [ `echo "${status}" | grep -E "${changes}" | wc -l` != 0 ]; then
-        echo -e "\E[31;1m$file\E[0m has changes.";
+        echo -en "\E[31;1m$file\E[0m has changes:";
 
-        #for line in "${status}"; do 
-        #  [[ "${line}" =~ \ *([MADCUR\?]{1,2})\ .+ ]]
-        #  echo "${BASH_REMATCH[1]}"
-        #done
+        local statuses='';
+                
+        while read -r line; do
+          [[ "${line}" =~ \ *([CAR\?MUD\!]{1,2})\ .+ ]]
+          statuses="${statuses} ${BASH_REMATCH[1]}" 
+        done <<< "${status}"
         
+        echo "${statuses}" | sed 's/ /\n/g' | sort | uniq | tr "\\n" " " # get all unique status values
+        echo ""
+        #statuses="`echo "${statuses}" | sed 's/ /\n/g' | sort | uniq`" # get all unique status values
+        #statuses="`echo "${statuses}" | sed 's/ /\n/g'`" # line per status
+        
+        #local repoStatus='';
+        
+        #while read -r line; do
+        #  [[ "${line}" =~ ([CAR\?MUD\!])+ ]]
+        #  local s="${BASH_REMATCH[1]}";
+        #  
+        #  case $s in
+        #    A) echo -n "added ";;
+        #    M) echo -n "modified ";;
+        #    D) echo -n "deleted ";;
+        #    R) echo -n "renamed ";;
+        #    C) echo -n "copied ";;
+        #    U) echo -n "updated ";;
+        #    ?) echo -n "untracked ";;
+        #  esac
+        #done <<< "${statuses}"
+        #
+        #echo "";
       fi
       
       if [ "${status}" == "" ]; then
